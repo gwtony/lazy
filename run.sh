778f4e011f0b58d28673ea110c6666f9631146fd
guanwei <guanwei@mininglamp.com>
2019-07-06 18:38:26 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 yum install -y wget &>/dev/null
 wget -q https://github.com/shadowsocks/shadowsocks-go/releases/download/1.2.1/shadowsocks-server.tar.gz
 tar zxvf shadowsocks-server.tar.gz &> /dev/null
-IP=`ip a | grep -Eo 'inet [.0-9]+' | awk '{print $2}' | grep -v 127.0.0.1`
+IP=`ip a s eth0 | grep -Eo 'inet [.0-9]+' | awk '{print $2}'`
 cat <<EOF > config.json
 {
     "server": "$IP",
